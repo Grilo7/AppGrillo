b72a89e7d79504b2a97ef99c465892a8e73792ce
Grilo7 <grillofoto@gmail.com>
2022-04-21 20:36:48 -0300

    Cambio en el texto

--- App.js
+++ App.js
@@ -4,10 +4,14 @@ import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
-      <Text style= {styles.text}>Hola, Coder!</Text>
+      <Text style= {styles.title}>Hola, Coder!</Text>
+      <Text style= {styles.text}>Bienvenidos al curso de desarrollo de aplicaciones. </Text>
       <StatusBar style="auto" />
     </View>
+    
+    </>
   );
 }
 
@@ -15,11 +19,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'right',
     justifyContent: 'center',
+  },
+    title: {
+
+    fontSize: 24,
+    fontWeight: 800,
+
   },
     text: {
 
-    fontSize: 24,
-  },
+      fontSize: 18,
+      fontWeight: 400,
+                        
+    },
 });
